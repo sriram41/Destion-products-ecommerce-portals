3ee86bf8c60481f28b6102bbda498dfb828a0006
sriram41 <133516424+sriram41@users.noreply.github.com>
2025-03-20 18:32:18 +0530

    Update server.js

--- destion-products-portals-master/server/server.js
+++ destion-products-portals-master/server/server.js
@@ -1,32 +1,32 @@
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-// const app = express();
+const app = express();
 
-// // Middleware
-// app.use(express.json());
-// app.use(cors({ origin: "https://sriram-khandavilli-destion-products-ecommerce-portals.vercel.app/login" }));
-
-
-
-
-// // Routes
-// app.use("/api/auth", require("./routes/authRoutes"));
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: "https://sriram-khandavilli-destion-products-ecommerce-portals.vercel.app/login" }));
 
 
 
-// app.get("/api/invoices", (req, res) => {
-//     const filePath = path.join(__dirname, "invoices.json");
-//     fs.readFile(filePath, "utf8", (err, data) => {
-//       if (err) {
-//         console.error("Error reading invoices:", err);
-//         return res.status(500).json({ message: "Internal Server Error" });
-//       }
-//       console.log("Invoices from Backend:", JSON.parse(data)); // 🐞 Debugging
-//       res.json(JSON.parse(data));
-//     });
-//   });
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+
+
+app.get("/api/invoices", (req, res) => {
+    const filePath = path.join(__dirname, "invoices.json");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading invoices:", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      console.log("Invoices from Backend:", JSON.parse(data)); // 🐞 Debugging
+      res.json(JSON.parse(data));
+    });
+  });
   
 
 
@@ -36,20 +36,20 @@
 
 
 
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+// const express = require("express");
+// const cors = require("cors");
+// const authRoutes = require("./routes/authRoutes");
 
-const app = express();
+// const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true })); // Allow requests from your React app
+// // Middleware
+// app.use(express.json());
+// app.use(cors({ origin: "http://localhost:3000", credentials: true })); // Allow requests from your React app
 
-// Routes
-app.use("/api/auth", authRoutes);
+// // Routes
+// app.use("/api/auth", authRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// // Start server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
